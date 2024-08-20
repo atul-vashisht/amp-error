2a8184be752e1c83d10fe8945cefa9e061878707
Atul Vashisht <guruaugust@gmail.com>
2024-08-20 15:04:13 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { useAmp } from "next/amp";
+
+
+export default function Home() {
+
+
+  // const jsonLd = {
+  //   '@context': 'https://schema.org',
+  //   '@type': 'Product',
+  //   name: 'product.name',
+  //   image: 'product.image',
+  //   description: 'product.description',
+  // }
+ 
+
+  return (
+    
+    
+    <>
+     <Link href="/amp_page"> I am nextjs link on non-AMP page</Link>,
+    </>
+      
+      
+      
+      
+  );
+}
